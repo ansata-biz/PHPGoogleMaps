f6961d43f3eaf907641fde84b370267b07d2411f
7heaven <planet.axis@gmail.com>
2013-02-14 20:13:33 +0200

    introduced unit system constants

--- Map.php
+++ Map.php
@@ -32,6 +32,9 @@ namespace PHPGoogleMaps;
 
 class Map {
 
+  const UNITS_IMPERIAL = 'IMPERIAL';
+  const UNITS_METRIC = 'METRIC';
+
 	/**
 	 * Map ID
 	 *
@@ -1228,7 +1231,7 @@ class Map {
 	 * @return void
 	 */
 	public function setUnitsMetric() {
-		$this->setUnits( 'METRIC' );
+		$this->setUnits( self::UNITS_METRIC );
 	}
 
 	/**
@@ -1237,7 +1240,7 @@ class Map {
 	 * @return void
 	 */
 	public function setUnitsImperial() {
-		$this->setUnits( 'IMPERIAL' );
+		$this->setUnits( self::UNITS_IMPERIAL );
 	}
 
 	/**
